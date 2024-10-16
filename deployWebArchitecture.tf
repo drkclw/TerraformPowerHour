85ce97300cd69020c0b40a66940fc41abc177dfb
Sam Gomez <Sam.Gomez@geneca.com>
2024-10-15 20:05:31 -0400

    Added main, sql server and log analytics workspace modules.

---
+++ deployWebArchitecture.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "rg-contact-web-application" {
+   name     = var.resource_group_name
+   location = var.location
+}
+
+module "sqlServer" {
+  source = "./modules/sqlServer"
+
+  resourceGroupName = azurerm_resource_group.rg-contact-web-application.name
+  location          = azurerm_resource_group.rg-contact-web-application.location
+  clientIpAddress   = var.clientIpAddress
+  sqlServerName     = var.sqlServerName
+  sqlServerAdmin    = var.sqlServerAdmin 
+  sqlServerPwd      = var.sqlServerPwd
+  sqlDatabaseName   = var.sqlDatabaseName
+  sqlDbSkuName      = var.sqlDbSkuName
+  uniqueIdentifier  = var.uniqueIdentifier
+}
+
+module "logAnalyticsWorkspace" {
+  source = "./modules/logAnalyticsWorkspace"
+
+  resourceGroupName         = azurerm_resource_group.rg-contact-web-application.name
+  location                  = azurerm_resource_group.rg-contact-web-application.location
+  logAnalyticsWorkSpaceName = var.logAnalyticsWorkSpaceName
+}
